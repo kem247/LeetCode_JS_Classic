d73aa0444aa2d98c3dac15c46b1594b7568fc377
Kemi Thomas <kemothom71@gmail.com>
2022-05-29 22:40:17 -0400

    LC125

--- exercises/LC125_isPalindrome/index.js
+++ exercises/LC125_isPalindrome/index.js
@@ -1,3 +1,23 @@
-function isPalindrome(s) {}
+function isPalindrome(s) {
+  //Sanitize the input string
+  //first arg in replace is what you want to replace
+  //Regex: a square bracket will represent a capture group
+  //in the brackets it says grab all non-alphanumeric characters
+  // _ is in cluding underscores
+  s = s.toLowerCase().replace(/[\W_]/g, '');
+
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return false;
+    }
+    //move the pointers or you'll be in an infinite loop
+    left++;
+    right--;
+  }
+  return true;
+}
 
 module.exports = isPalindrome;
